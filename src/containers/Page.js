24498fc6cc63f5f1c9131de0ccab47f4df34cf1f
alik0211 <alik0211alik@gmail.com>
2018-06-26 09:06:51 +0400

    Move JSX from containers to components

--- src/containers/Page.js
+++ src/containers/Page.js
@@ -1,38 +1,7 @@
-import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import Pokemon from '../components/Pokemon'
-import Search from '../components/Search'
 import * as pageActions from '../actions/PageActions'
-
-class Page extends Component {
-  componentDidMount() {
-    this.props.pageActions.fetchPokemons()
-  }
-
-  handleSearch(event) {
-    this.props.pageActions.filterPokemons(event.target.value)
-  }
-
-  render() {
-    let { displayedPokemons, isFetched } = this.props.page
-
-    let pokemons = displayedPokemons.map(pokemon => {
-      return (
-        <li className="pokemons__item" key={pokemon.id}>
-          <Pokemon pokemon={pokemon} />
-        </li>
-      )
-    })
-
-    return (
-      <div className="page">
-        <Search onChange={this.handleSearch.bind(this)} />
-        <ul className="pokemons">{isFetched ? <p>Loading...</p> : pokemons}</ul>
-      </div>
-    )
-  }
-}
+import Page from '../components/Page'
 
 function mapStateToProps(state) {
   return {
