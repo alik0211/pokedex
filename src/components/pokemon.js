f9adc39c9e47d3cd5e4ab351b611d2e8ca1f6b48
Ali Gasymov <alik0211alik@gmail.com>
2018-06-29 12:46:07 +0400

    Optimize your pokémon component
    PureComponent does not rerender a component unless it has changed props
    
    More: https://reactjs.org/docs/react-api.html#reactpurecomponent

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 
-const Pokemon = ({ pokemon }) => (
-  <div className="pokemon">
-    <button
-      type="button"
-      className="pokemon__sprite"
-      style={{
-        backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-          pokemon.id
-        }.png`})`
-      }}
-    />
-    <p className="pokemon__name">{pokemon.name}</p>
-  </div>
-)
+class Pokemon extends PureComponent {
+  render() {
+    const { pokemon } = this.props
+
+    return (
+      <div className="pokemon">
+        <button
+          type="button"
+          className="pokemon__sprite"
+          style={{
+            backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              pokemon.id
+            }.png`})`
+          }}
+        />
+        <p className="pokemon__name">{pokemon.name}</p>
+      </div>
+    )
+  }
+}
 
 export default Pokemon
