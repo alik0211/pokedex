90aeaa85d82772d3c17d1eb2dcf83b32072c4022
Ofir Attia <OFIRATT@amdocs.com>
2018-12-05 12:22:18 +0200

    change test

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ describe('Should Component Contain Object', function() {
   describe('#Pokemon Component', function() {
     it('moreInfo property should be as object', function() {
       var P = new Pokemon()
-      assert.deepEqual(P.state.moreInfo.constructor, Object)
+      assert.deepEqual(P.state.moreInfo.constructor, Array)
     })
   })
 })
