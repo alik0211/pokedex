49a40110542a98cf75e07c2d9858ff5e4b3ce5f4
Ali Gasymov <alik0211alik@gmail.com>
2018-12-05 10:31:14 +0400

    Add autoFocus into search (#7)

--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Search = ({ onChange }) => (
-  <input type="text" onChange={onChange} placeholder="Enter pokemon name..." />
+  <input
+    type="text"
+    onChange={onChange}
+    placeholder="Enter pokemon name..."
+    autoFocus
+  />
 )
 
 export default Search
