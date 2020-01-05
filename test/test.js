02b53424e000d9d11b325a5d11d8a38a85ef5be1
ofiratt <Ofir.Attia@amdocs.com>
2020-01-05 19:43:21 +0200

    adding semicolon in the test file

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ import Pokemon from '../src/components/pokemon'
 describe('Should Component Contain Object', function() {
   describe('#Pokemon Component', function() {
     it('moreInfo property should be as object', function() {
-      var P = new Pokemon()
-      assert.deepEqual(P.state.moreInfo.constructor, Object)
-    })
-  })
-})
+      var P = new Pokemon();
+      assert.deepEqual(P.state.moreInfo.constructor, Object);
+    });
+  });
+});
