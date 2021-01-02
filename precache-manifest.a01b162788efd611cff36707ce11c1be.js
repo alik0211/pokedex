266f5629ed46fe0a3f08107cedc51a2b1b6d4105
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-02 11:05:51 +0000

    Deploy alik0211/pokedex to github.com/alik0211/pokedex.git:gh-pages

---
+++ precache-manifest.a01b162788efd611cff36707ce11c1be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "0ed9863ec0ac0d2572ce",
+    "url": "/pokedex/static/js/main.23a78fa7.chunk.js"
+  },
+  {
+    "revision": "ed313fa7ca123623df3e",
+    "url": "/pokedex/static/js/2.a825d548.chunk.js"
+  },
+  {
+    "revision": "0ed9863ec0ac0d2572ce",
+    "url": "/pokedex/static/css/main.c2dc6016.chunk.css"
+  },
+  {
+    "revision": "c9252191f2a8c09748a5e982e75fcec3",
+    "url": "/pokedex/index.html"
+  }
+];
